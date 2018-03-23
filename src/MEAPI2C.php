5379f594666a72b507a082cc4edec9741fc92eb4
zinntikumugai <uesitananame55@gmail.com>
2018-03-23 20:18:11 +0900

    add SelfLoader 	modified:   src/MEAPI2C.php

--- src/MEAPI2C.php
+++ src/MEAPI2C.php
@@ -1,5 +1,31 @@
 <?php
 namespace MEAPI2C;
 class MEAPI2C {
+    const MODEL = [
+        "\MEAPI2C\Model\zaif",
+        "\MEAPI2C\Model\ccex"
+    ];
 
+    private $loadModel = [];
+
+    public function __construct() {
+        foreach(self::MODEL as $model) {
+            $keylist = explode('\\',$model);
+            $key = $keylist[count($keylist)-1];
+            $this->loadModel[$key] = [
+                'name' => $model::NAME,
+                'class' => $model
+            ];
+        }
+    }
+
+    public function supportList() {
+        return $this->loadModel;
+    }
+
+    public function selecter($exName = ""){
+        if(!array_key_exists($exName, $this->loadModel))
+            return false;
+        return $this->loadModel[$exName];
+    }
 }
